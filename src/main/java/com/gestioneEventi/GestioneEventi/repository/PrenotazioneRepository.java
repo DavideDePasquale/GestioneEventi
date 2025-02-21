d786f08d24b163efabb301fbe321d0b4cc700503
Davide De Pasquale <davide.depasquale1998@gmail.com>
2025-02-21 16:19:54 +0100

    utente ok

--- src/main/java/com/gestioneEventi/GestioneEventi/repository/PrenotazioneRepository.java
+++ src/main/java/com/gestioneEventi/GestioneEventi/repository/PrenotazioneRepository.java
@@ -4,6 +4,11 @@ import com.gestioneEventi.GestioneEventi.model.Prenotazione;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PrenotazioneRepository extends JpaRepository<Prenotazione,Long> {
+
+    public Optional<Prenotazione> findById(Long id);
+
 }
--- src/main/java/com/gestioneEventi/GestioneEventi/repository/UtenteRepository.java
+++ src/main/java/com/gestioneEventi/GestioneEventi/repository/UtenteRepository.java
@@ -4,6 +4,16 @@ import com.gestioneEventi.GestioneEventi.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UtenteRepository extends JpaRepository<Utente,Long> {
+
+    public Optional<Utente> findByUsername(String username);
+    // x la login
+    public boolean existsByUsernameAndPassword(String username, String password);
+    // per visualizzare alla creazione se l'username è libero (quindi se è presente o no nel db)
+    public boolean existsByUsername(String username);
+    // per visualizzare alla creazione se la mail è già presente o meno sul db
+    public boolean existsByEmail(String email);
 }
